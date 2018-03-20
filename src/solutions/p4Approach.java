3ebce72e1608d717a0244c1927a951324fb83abd
victor-nazario <33332105+victor-nazario@users.noreply.github.com>
2018-03-20 19:40:39 -0400

    Necessary updates

--- src/solutions/p4Approach.java
+++ src/solutions/p4Approach.java
@@ -3,7 +3,7 @@ package solutions;
 import interfaces.MySet;
 import setIntersectionFinders.AbstractIntersectionFinder;
 
-public class p4Approach extends AbstractIntersectionFinder{
+public class p4Approach extends P3Approach{
 
 	public p4Approach(String name) {
 		super(name);
