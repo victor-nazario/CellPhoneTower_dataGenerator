f5c6cc988f04d48481429821345799309d62ac95
victor-nazario <33332105+victor-nazario@users.noreply.github.com>
2018-03-20 19:26:10 -0400

    Added some necessary classes

---
+++ src/solutions/P1_P2Approach.java
@@ -0,0 +1,19 @@
+package solutions;
+
+import interfaces.MySet;
+import setIntersectionFinders.AbstractIntersectionFinder;
+
+public class P1_P2Approach extends AbstractIntersectionFinder{
+
+	public P1_P2Approach(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public MySet intersectSets(MySet[] t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/solutions/p4Approach.java
@@ -0,0 +1,19 @@
+package solutions;
+
+import interfaces.MySet;
+import setIntersectionFinders.AbstractIntersectionFinder;
+
+public class p4Approach extends AbstractIntersectionFinder{
+
+	public p4Approach(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public MySet intersectSets(MySet[] t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
