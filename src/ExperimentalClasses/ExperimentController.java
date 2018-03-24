a200d6dcf1bf41371a520f4ad2041d3a6e4bcf14
victor-nazario <33332105+victor-nazario@users.noreply.github.com>
2018-03-23 22:13:22 -0400

    Final commit

---
+++ src/ExperimentalClasses/ExperimentController.java
@@ -0,0 +1,126 @@
+package ExperimentalClasses;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import dataGenerator.DataGenerator;
+
+/**
+ * This class represents an object data type that is able to carry the 
+ * necessary experiments to estimate execution times of particular 
+ * strategies to solve the set intersections problem as in p1_40354020_172. 
+ * 
+ * @author pedroirivera-vega
+ * 
+ * 
+ * Adjusted for project 1 by:
+ * @author Víctor A. Nazario Morales 843-15-4984 S-050
+ * @author Jairo Rosado Soto 844-12-7240 S-070
+ *
+ */
+
+public class ExperimentController{
+	
+	private int n = 10;					//number of telephone companies
+	private int m = 50;					//number of crime events
+	private int initialSize = 1000;           // initial size to be tested
+	private int finalSize = 50000;             // last size to be tested
+	private int incrementalSizeStep = 1000;   // change of sizes
+	private int repetitionsPerSize = 200;    // experimental repetitions per size
+	
+	private ArrayList<StrategiesTimeCollection<Integer>> resultsPerStrategy; 
+	// The i-th position will contain a particular strategy being tested. 
+	// At the end, the i-th position will also contain a list of 
+	// pairs (n, t), where t is the estimated time for size n for
+	// the strategy at that position. 
+	
+	public ExperimentController(int n, int m, int is, int fs, int iss, int rps) {
+		this.n = n;
+		this.m = m;
+		initialSize = is;  
+		finalSize = fs;
+		incrementalSizeStep = iss; 
+		repetitionsPerSize = rps;
+		resultsPerStrategy = new ArrayList<>(); 
+	}
+	
+	public void addStrategy(StrategiesTimeCollection<Integer> strategy) { 
+		resultsPerStrategy.add(strategy); 
+	}
+
+	public void run() throws CloneNotSupportedException { 
+		if (resultsPerStrategy.isEmpty())
+			throw new IllegalStateException("No strategy has been added."); 
+		for (int size=initialSize; size<=finalSize; size+=incrementalSizeStep) { 
+			// For each strategy, reset the corresponding variable that will be used
+			// to store the sum of times that the particular strategy exhibits for
+			// the current size size
+			for (StrategiesTimeCollection<Integer> strategy : resultsPerStrategy) 
+				strategy.resetSum();  
+			
+			// Run all trials for the current size. 
+			for (int r = 0; r<repetitionsPerSize; r++) {
+				// The following will be the common dataset to be used in the current 
+				// trial by all the strategies being tested.
+				Object[][][] dataSet = generateData(n, m, size);  
+				
+				// Apply each one of the strategies being tested using the previous 
+				// dataset (of size size) as input; and, for each, estimate the time
+				// that the execution takes. 
+				for (StrategiesTimeCollection<Integer> strategy : resultsPerStrategy) {  
+					// no need to clone the data set to be used by each strategy since
+					// no modification of it is done in the process...
+					long startTime = System.nanoTime(); // System.currentTimeMillis();   // time before
+
+					strategy.runTrial(dataSet.clone());   // run the particular strategy...
+					
+					long endTime = System.nanoTime(); // System.currentTimeMillis();    // time after
+
+					// accumulate the estimated time (add it) to sum of times that
+					// the current strategy has exhibited on trials for datasets
+					// of the current size.
+					
+					strategy.incSum((int) (endTime-startTime));  //estimate time  
+					
+				}
+			}
+			
+			for (StrategiesTimeCollection<Integer> strategy : resultsPerStrategy) { 
+				strategy.add(new AbstractMap.SimpleEntry<Integer, Float>
+				(size, (strategy.getSum()/((float) repetitionsPerSize)))); 
+			}
+
+			System.out.println(size); 
+
+		}
+	}
+	
+	private Object[][][] generateData(int n, int m, int size) {
+		DataGenerator dg = new DataGenerator(n, m, size);
+		Object[][][] dataSet = dg.generateData();  
+
+		return dataSet;
+	}
+
+	public void saveResults() throws FileNotFoundException { 
+		
+		PrintStream out = new PrintStream(new File("experimentalResults", "allResults.txt"));
+		out.print("Size");
+		for (StrategiesTimeCollection<Integer> trc : resultsPerStrategy) 
+			out.print("\t" + trc.getStrategyName()); 
+		out.println();
+
+		int numberOfExperiments = resultsPerStrategy.get(0).size(); 
+		for (int i=0; i<numberOfExperiments; i++) {
+			out.print(resultsPerStrategy.get(0).get(i).getKey());
+			for (StrategiesTimeCollection<Integer> trc : resultsPerStrategy)
+				out.print("\t" + trc.get(i).getValue());
+			out.println(); 
+		}
+			
+		out.close();
+		
+	}
+}
--- src/p1MainClasses/Part1Main.java
+++ src/p1MainClasses/Part1Main.java
@@ -1,6 +1,13 @@
 
 package p1MainClasses;
 
+/**
+*
+* @author Víctor A. Nazario Morales 843-15-4984 S-050
+* @author Jairo Rosado Soto 844-12-7240 S-070
+*
+*/
+
 import java.io.FileNotFoundException;
 import dataGenerator.DataReader;
 import interfaces.IntersectionFinder;
@@ -11,18 +18,12 @@ import solutions.P1_P2Approach;
 import solutions.P3Approach;
 import solutions.P4Approach;
 
-/**
- *
- * @author Víctor A. Nazario Morales 843-15-4984 S-050
- * @author Jairo Rosado Soto 844-12-7240 S-070
- *
- */
 
 public class Part1Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		if (args.length > 1)
-			System.out.println("Unexpected number of parameters. Must be 1.");
+			System.out.println("Parameter number must equal one");
 		
 		//Create an array of intersection finders 
 		IntersectionFinder[] intFind = new IntersectionFinder[4]; 
@@ -60,7 +61,7 @@ public class Part1Main {
 		MySet[] finalSet = new MySet[dataSet[0].length];
 
 		for(int j = 0 ; j<dataSet[0].length ; j++) {
-			finalSet[j] = new Set2();                           //initializes the set as Set2
+			finalSet[j] = new Set2();                               //initializes the set as Set2
 			if(argumentLine.equals("1")) finalSet[j] = new Set1();  //if the args is "1" then it is changed to Set1
 			for(int i =0 ; i<dataSet.length ; i++) {
 				for( int k=0 ; k< dataSet[i][j].length; k++) {
@@ -71,174 +72,3 @@ public class Part1Main {
 		return finalSet;
 	}
 }
-
-
-
-//package p1MainClasses;
-//
-//import java.io.FileNotFoundException;
-//import dataGenerator.DataReader;
-//import interfaces.MySet;
-//import mySetImplementations.Set1;
-//import mySetImplementations.Set2;
-//import solutions.P1_P2Approach;
-//import solutions.P3Approach;
-//import solutions.P4Approach;
-//
-//public class Part1Main {
-//
-//	public static void main(String[] args) {
-//		
-//		try {
-//			DataReader dr = new DataReader();
-//			Integer[][][] theArr = (Integer[][][]) dr.readDataFiles(); 
-//			int eventNum = theArr[0].length;
-//			int companyNum = theArr.length;
-//			MySet[] arr1 = new Set1[eventNum]; 
-//			MySet[] arr2 = new Set2[eventNum];
-//			MySet<Integer>[] theSet = new MySet[eventNum];
-//			for (int j = 0; j < eventNum; j++) {
-//			
-//			
-//				if (args[0] == "1") {
-//					theSet[j] = new Set1<Integer>(); 
-//				}
-//				
-//				else {
-//					theSet[j] = new Set2<Integer>(); 
-//				}
-//				
-//				for (int i = 0; i < companyNum; i++) {
-//					for (int k = 0; k < theArr[i][k].length; k++) {
-//						theSet[j].add((Integer) theArr[i][j][k]);
-//					}
-//				}
-//				
-//	
-//			}
-//			
-//			
-//			if (args[0] == "1") {
-//				P1_P2Approach P1Trial = new P1_P2Approach("1"); 
-//				System.out.println("Final set by P4: " + P1Trial.intersectSets(theSet));
-//				
-//			}
-//			
-//			else if (args[0] == "2") {
-//				P1_P2Approach P2Trial = new P1_P2Approach("2"); 
-//				System.out.println("Final set by P2: " + P2Trial.intersectSets(arr2));
-//				
-//			}
-//			
-//			else if(args[0] == "3") {
-//				P3Approach P3Trial = new P3Approach("3"); 
-//				System.out.println("Final set by P3: " + P3Trial.intersectSets(arr2));
-//			}
-//			
-//			
-//			else if(args[0] == "4") {
-//				P4Approach  P4Trial = new P4Approach("4");
-//				System.out.println("Final set by P4: " + P4Trial.intersectSets(arr2));
-//			}
-//
-//			
-//			
-//		} catch (FileNotFoundException e1) {
-//			
-//			e1.printStackTrace();
-//		}
-//
-//		
-//	}
-//
-//	
-//}
-//	
-//	
-//	
-	
-	/** The solutionRuner method switches between the four proposed solutions to apply to each data entry
-	 * @param solution, a value ranged from 1-4 to select which person's solution to use.
-	 */
-	
-//	public static void solutonRuner(int solution) {
-////		MySet[] t = (MySet[]) new Object[5000];    //verificar para remover
-////		int num;
-//		
-//		try {
-//			DataReader dr = new DataReader();
-//			Object[][][] theArr = dr.readDataFiles(); 
-//			int eventNum = theArr[0].length;
-//			int companyNum = theArr.length;
-//			MySet[] arr1 = new Set1[eventNum]; 
-//			MySet[] arr2 = new Set2[eventNum];
-//			
-//			for (int j = 0; j < eventNum; j++) {
-//				MySet<Integer> theSet1 = new Set1<>(); 
-//				MySet<Integer> theSet2 = new Set2<>(); 
-//				for (int i = 0; i < companyNum; i++) {
-//					for (int k = 0; k < theArr[i][k].length; k++) {
-//						theSet1.add((Integer) theArr[i][j][k]);
-//						theSet2.add((Integer) theArr[i][j][k]);
-//					}
-//				}
-//				arr1[j] = theSet1;
-//				arr2[j] = theSet2; 
-//			}
-//			
-//			
-//			if (solution == 1) {
-//				P1_P2Approach P1Trial = new P1_P2Approach("1"); 
-//				System.out.println("Final set by P4: " + P1Trial.intersectSets(arr1));
-//				
-//			}
-//			
-//			else if (solution == 2) {
-//				P1_P2Approach P2Trial = new P1_P2Approach("2"); 
-//				System.out.println("Final set by P2: " + P2Trial.intersectSets(arr2));
-//				
-//			}
-//			
-//			else if(solution == 3) {
-//				P3Approach P3Trial = new P3Approach("3"); 
-//				System.out.println("Final set by P3: " + P3Trial.intersectSets(arr2));
-//			}
-//			
-//			
-//			else if(solution == 4) {
-//				P4Approach  P4Trial = new P4Approach("4");
-//				System.out.println("Final set by P4: " + P4Trial.intersectSets(arr2));
-//			}
-//
-//			
-//			
-//		} catch (FileNotFoundException e1) {
-//			
-//			e1.printStackTrace();
-//		}
-//
-//		
-//	}
-//
-//
-//}
-
-
-
-//Set[] atr = (Set[]) new Object[5000]; 
-//
-//if (solution == 1) {
-//	P1_P2Approach p1_p2Trial = new P1_P2Approach("1"); 
-//	
-//}
-//
-//else if(solution == 2) {
-//	P3Approach P3Trial = new P3Approach("2"); 
-//}
-//
-//else if(solution == 3) {
-//	P4Approach  P4Trial = new P4Approach("3");
-//}
-
-
-
